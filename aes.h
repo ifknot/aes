042390bb581bb92344d1f4fd4f11802ddd334b7d
ifknot <ifknot@hotmail.com>
2019-05-20 22:16:00 +0100

    convert from state pointer to block_t reference - not any slower! teeny bit faster... nice

--- aes.h
+++ aes.h
@@ -3,12 +3,7 @@
 
 #include <array>
 
-//TODO get rid
-#define getSBoxValue(num) (sbox[(num)])
-
-namespace crypto {
-    //TODO is it more C++17 style to use lambdas for add_round_key, sub_bytes, shift_rows & mix_columns?
-    //TODO will lamdas affect performance?
+namespace pug::crypto {
 
     /**
      * R as the round number - round keys needed:
@@ -43,11 +38,11 @@ namespace crypto {
 
         /**
          * 128-bit block size.
-         * Until the announcement of NIST's AES contest, the majority of block cipsub_byteshers followed the example of the DES in using a block size of 64 bits (8 bytes).
+         * Until the announcement of NIST's AES contest, the majority of block ciphers followed the example of the DES in using a block size of 64 bits (8 bytes).
          * However, the birthday paradox tells us that after accumulating a number of blocks equal to the square root of the total number possible,
          * there will be an approximately 50% chance of two or more being the same, which would start to leak information about the message contents.
          * Consequently, AES candidates were required to support a block length of 128 bits (16 bytes).
-         * This should be acceptable for up to 2^64mix_columns × 16 B = 256 exabytes of data - which should suffice for quite a few years to come.
+         * This should be acceptable for up to 2^64 × 16 B = 256 exabytes of data - which should suffice for quite a few years to come.
          * The winner of the AES contest, Rijndael, supports block and key sizes of 128, 192, and 256 bits, but in AES the block size is always 128 bits.
          */
         constexpr static size_t BLOCK_SIZE = 16;
@@ -77,13 +72,13 @@ namespace crypto {
 
     public:
 
+        using value_type = T;
         using block_t = std::array<T, BLOCK_SIZE>;
-        using state_t = T[4][4];
 
         template<class Sequence>
         explicit aes(Sequence&& seq) noexcept;
 
-        aes(aes const&) = delete;mix_columns
+        aes(aes const&) = delete;
 
         aes& operator=(aes const&) = delete;
 
@@ -94,32 +89,22 @@ namespace crypto {
          * @param p the plaintext(state_t*)buf
          * @return 16 byte block of ciphertext
          */
-        block_t encrypt(state_t* state) {//TODO const block_t&
+        block_t encrypt(block_t& block) {
             uint8_t round = 0;
-            // Add the First round key to the state before starting the rounds.
-            //AddRoundKey(round, state);
-            add_round_key(round, (uint8_t*)state);
-            // The first R - 1 rounds are identical.
-            // These R - 1 rounds are executed in the loop below.
+            // add the First round key to the state before starting the rounds.
+            add_round_key(round, block);
+            // first R - 1 rounds are identical...
             auto i = R - 1;
             while(--i) {
-                //SubBytes(state);
-                sub_bytes((uint8_t*)state);
-                //ShiftRows(state); // Rijndael diffusion
-                shift_rows((uint8_t*)state);
-                //MixColumns(state);
-                mix_columns((uint8_t*)state); // Rijndael diffusion
-                //AddRoundKey(round, state);
-                add_round_key(round, (uint8_t*)state);
+                sub_bytes(block);
+                shift_rows(block); // Rijndael diffusion
+                mix_columns(block); // Rijndael diffusion
+                add_round_key(round, block);
             }
-            // The last round is given below.
-            // The MixColumns function is not here in the last round.
-            //SubBytes(state);
-            sub_bytes((uint8_t*)state);
-            //ShiftRows(state);
-            shift_rows((uint8_t*)state);
-            //AddRoundKey(round, state);
-            add_round_key(round, (uint8_t*)state);
+            // final round lacks mix_columns diffusion
+            sub_bytes(block);
+            shift_rows(block);
+            add_round_key(round, block);
         }
 
         /**
@@ -139,146 +124,88 @@ namespace crypto {
 
     private:
 
-        // This function adds the round key to state.
-        // The round key is added to the state by an XOR function.
-        void AddRoundKey(uint8_t round, state_t* state) {
-            uint8_t i,j;
-            for (i = 0; i < 4; ++i)
-            {
-                for (j = 0; j < 4; ++j)
-                {
-                    (*state)[i][j] ^= xkey[(round * 4 * 4) + (i * 4) + j];
-                }
-            }
+        inline void add_round_key(uint8_t& round, block_t& block) {
+            size_t i{0};
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
+            block[i++] ^= xkey[round++];
         }
 
-        void add_round_key(uint8_t& round, uint8_t* state) {
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
-            *(state++) ^= xkey[round++];
+        /**
+         * @brief S-box substitution
+         * @param state
+         */
+        inline void sub_bytes(block_t& block) {
+            size_t i{0};
+            block[i + 0 ]  = sbox[ block[i + 0 ] ];
+            block[i + 1 ]  = sbox[ block[i + 1 ] ];
+            block[i + 2 ]  = sbox[ block[i + 2 ] ];
+            block[i + 3 ]  = sbox[ block[i + 3 ] ];
+            block[i + 4 ]  = sbox[ block[i + 4 ] ];
+            block[i + 5 ]  = sbox[ block[i + 5 ] ];
+            block[i + 6 ]  = sbox[ block[i + 6 ] ];
+            block[i + 7 ]  = sbox[ block[i + 7 ] ];
+            block[i + 8 ]  = sbox[ block[i + 8 ] ];
+            block[i + 9 ]  = sbox[ block[i + 9 ] ];
+            block[i + 10]  = sbox[ block[i + 10] ];
+            block[i + 11]  = sbox[ block[i + 11] ];
+            block[i + 12]  = sbox[ block[i + 12] ];
+            block[i + 13]  = sbox[ block[i + 13] ];
+            block[i + 14]  = sbox[ block[i + 14] ];
+            block[i + 15]  = sbox[ block[i + 15] ];
         }
 
-        // The SubBytes Function Substitutes the values in the
-        // state matrix with values in an S-box.
-        void SubBytes(state_t* state) {
-            uint8_t i, j;
-            for (i = 0; i < 4; ++i)
-            {
-                for (j = 0; j < 4; ++j)
-                {
-                    (*state)[j][i] = getSBoxValue((*state)[j][i]);
-                }
-            }
-        }
-
-        void sub_bytes(uint8_t* state) {
-            *(state + 0 ) = sbox[*(state + 0 )];
-            *(state + 1 ) = sbox[*(state + 1 )];
-            *(state + 2 ) = sbox[*(state + 2 )];
-            *(state + 3 ) = sbox[*(state + 3 )];
-            *(state + 4 ) = sbox[*(state + 4 )];
-            *(state + 5 ) = sbox[*(state + 5 )];
-            *(state + 6 ) = sbox[*(state + 6 )];
-            *(state + 7 ) = sbox[*(state + 7 )];
-            *(state + 8 ) = sbox[*(state + 8 )];
-            *(state + 9 ) = sbox[*(state + 9 )];
-            *(state + 10) = sbox[*(state + 10)];
-            *(state + 11) = sbox[*(state + 11)];
-            *(state + 12) = sbox[*(state + 12)];
-            *(state + 13) = sbox[*(state + 13)];
-            *(state + 14) = sbox[*(state + 14)];
-            *(state + 15) = sbox[*(state + 15)];
-        }
-
-        // The ShiftRows() function shifts the rows in the state to the left.
-// Each row is shifted with different offset.
-// Offset = Row number. So the first row is not shifted.
-        void ShiftRows(state_t* state)
-        {
-            uint8_t temp;
-
-            // Rotate first row 1 columns to left
-            temp           = (*state)[0][1];
-            (*state)[0][1] = (*state)[1][1];
-            (*state)[1][1] = (*state)[2][1];
-            (*state)[2][1] = (*state)[3][1];
-            (*state)[3][1] = temp;
-
-            // Rotate second row 2 columns to left
-            temp           = (*state)[0][2];
-            (*state)[0][2] = (*state)[2][2];
-            (*state)[2][2] = temp;
-
-            temp           = (*state)[1][2];
-            (*state)[1][2] = (*state)[3][2];
-            (*state)[3][2] = temp;
-
-            // Rotate third row 3 columns to left
-            temp           = (*state)[0][3];
-            (*state)[0][3] = (*state)[3][3];
-            (*state)[3][3] = (*state)[2][3];
-            (*state)[2][3] = (*state)[1][3];
-            (*state)[1][3] = temp;
-        }
-
-        void shift_rows(uint8_t* state) {
+        /**
+         * @brief shifts the rows in the state to the left, each by a different offset.
+         * @param state
+         */
+        inline void shift_rows(block_t& block) {
             uint8_t rol;
+            size_t i{0};
             // Rotate first row 1 columns to left
-            rol = *(state + 1);
-            *(state + 1) = *(state + 5);
-            *(state + 5) = *(state + 9);
-            *(state + 9) = *(state + 13);
-            *(state + 13) = rol;
+            rol = block[i + 1];
+            block[i + 1 ]= block[i + 5 ];
+            block[i + 5 ]= block[i + 9 ];
+            block[i + 9 ]= block[i + 13];
+            block[i + 13]= rol;
             // Rotate second row 2 columns to left
-            rol = *(state + 2);
-            *(state + 2) = *(state + 10);
-            *(state + 10) = rol;
-            rol = *(state + 6);
-            *(state + 6) = *(state + 14);
-            *(state + 14) = rol;
+            rol = block[i + 2];
+            block[i + 2 ]= block[i + 10];
+            block[i + 10]= rol;
+            rol = block[i + 6];
+            block[i + 6 ]= block[i + 14];
+            block[i + 14]= rol;
             // Rotate third row 3 columns to left
-            rol = *(state + 3);
-            *(state + 3) = *(state + 15);
-            *(state + 15) = *(state + 11);
-            *(state + 11) = *(state + 7);
-            *(state + 7) = rol;
+            rol = block[i + 3];
+            block[i + 3 ]= block[i + 15];
+            block[i + 15]= block[i + 11];
+            block[i + 11]= block[i + 7 ];
+            block[i + 7 ]= rol;
         }
 
-        //multiplied by 2 in Rijndael's Galois field
-        uint8_t GF2(uint8_t x) {
+        /**
+         * multiply by 2 in the Rijndael algo's Galois field
+         * @param x
+         * @return
+         */
+        inline uint8_t GF2(T x){
             return (x<<1)           //implicitly removes high bit because 8-bit, (so * 0x1b and not 0x11b)
-                   ^               //xor
-                   (((x>>7) & 1)   // arithmetic right shift, thus shifting in either zeros or ones
+                   ^                //xor
+                   (((x>>7) & 1)    // arithmetic right shift, thus shifting in either zeros or ones
                     * 0x1b);        // Rijndael's Galois field
-        }
-
-
-        // MixColumns function mixes the columns of the state matrix
-        void MixColumns(state_t* state) {
-            uint8_t i;
-            uint8_t Tmp, Tm, t;
-            for (i = 0; i < 4; ++i)
-            {
-                t   = (*state)[i][0];
-                Tmp = (*state)[i][0] ^ (*state)[i][1] ^ (*state)[i][2] ^ (*state)[i][3] ;
-                Tm  = (*state)[i][0] ^ (*state)[i][1] ; Tm = GF2(Tm);  (*state)[i][0] ^= Tm ^ Tmp ;
-                Tm  = (*state)[i][1] ^ (*state)[i][2] ; Tm = GF2(Tm);  (*state)[i][1] ^= Tm ^ Tmp ;
-                Tm  = (*state)[i][2] ^ (*state)[i][3] ; Tm = GF2(Tm);  (*state)[i][2] ^= Tm ^ Tmp ;
-                Tm  = (*state)[i][3] ^ t ;              Tm = GF2(Tm);  (*state)[i][3] ^= Tm ^ Tmp ;
-            }
         }
 
         /**
@@ -287,68 +214,68 @@ namespace crypto {
          * elements of _GF(2^8)_. The modulo used for this operation is _x^4+1_.
          * @param state
          */
-        void mix_columns(uint8_t* state) {
+        inline void mix_columns(block_t& block) {
             uint8_t c, b, a;
+            size_t i{0};
+            a = block[i + 0];
+            c = block[i + 0] ^ block[i + 1] ^ block[i + 2 ] ^ block[i + 3];
+            b = block[i + 0] ^ block[i + 1];
+            b = GF2(b);
+            block[i + 0] ^= b ^ c;
+            b = block[i + 1] ^ block[i + 2];
+            b = GF2(b);
+            block[i + 1] ^= b ^ c;
+            b = block[i + 2] ^ block[i + 3];
+            b = GF2(b);
+            block[i + 2] ^= b ^ c;
+            b = block[i + 3] ^ a ;
+            b = GF2(b);
+            block[i + 3] ^= b ^ c;
 
-            a   = *(state + 0);
-            c = *(state + 0) ^ *(state + 1) ^ *(state + 2) ^ *(state + 3);
-            b  = *(state + 0) ^ *(state + 1);
+            a = block[i + 4];
+            c = block[i + 4] ^ block[i + 5 ] ^ block[i + 6 ] ^ block[i + 7];
+            b = block[i + 4] ^ block[i + 5];
             b = GF2(b);
-            *(state + 0) ^= b ^ c;
-            b  = *(state + 1) ^ *(state + 2);
+            block[i + 4] ^= b ^ c;
+            b = block[i + 5] ^ block[i + 6];
             b = GF2(b);
-            *(state + 1) ^= b ^ c;
-            b  = *(state + 2) ^ *(state + 3);
+            block[i + 5] ^= b ^ c;
+            b = block[i + 6] ^ block[i + 7];
             b = GF2(b);
-            *(state + 2) ^= b ^ c;
-            b  = *(state + 3) ^ a ;
+            block[i + 6] ^= b ^ c;
+            b = block[i + 7] ^ a ;
             b = GF2(b);
-            *(state + 3) ^= b ^ c;
+            block[i + 7] ^= b ^ c;
 
-            a   = *(state + 4);
-            c = *(state+ 4) ^ *(state+ 5) ^ *(state+ 6) ^ *(state + 7);
-            b  = *(state + 4) ^ *(state + 5);
+            a = block[i + 8];
+            c = block[i + 8] ^ block[i + 9] ^ block[i + 10] ^ block[i + 11];
+            b = block[i + 8] ^ block[i + 9];
             b = GF2(b);
-            *(state + 4) ^= b ^ c;
-            b  = *(state + 5) ^ *(state + 6);
+            block[i + 8] ^= b ^ c;
+            b = block[i + 9] ^ block[i + 10];
             b = GF2(b);
-            *(state + 5) ^= b ^ c;
-            b  = *(state + 6) ^ *(state + 7);
+            block[i + 9] ^= b ^ c;
+            b = block[i + 10] ^ block[i + 11];
             b = GF2(b);
-            *(state + 6) ^= b ^ c;
-            b  = *(state + 7) ^ a ;
+            block[i + 10] ^= b ^ c;
+            b = block[i + 11] ^ a ;
             b = GF2(b);
-            *(state + 7) ^= b ^ c;
+            block[i + 11] ^= b ^ c;
 
-            a   = *(state + 8);
-            c = *(state + 8) ^ *(state + 9) ^ *(state + 10) ^ *(state + 11);
-            b  = *(state + 8) ^ *(state + 9);
+            a = block[i + 12];
+            c = block[i + 12] ^ block[i + 13] ^ block[i + 14] ^ block[i + 15];
+            b = block[i + 12] ^ block[i + 13];
             b = GF2(b);
-            *(state + 8) ^= b ^ c;
-            b  = *(state + 9) ^ *(state + 10);
+            block[i + 12] ^= b ^ c;
+            b = block[i + 13] ^ block[i + 14];
             b = GF2(b);
-            *(state + 9) ^= b ^ c;
-            b  = *(state + 10) ^ *(state + 11);
+            block[i + 13] ^= b ^ c;
+            b = block[i + 14] ^ block[i + 15];
             b = GF2(b);
-            *(state + 10) ^= b ^ c;
-            b  = *(state + 11) ^ a ;
+            block[i + 14] ^= b ^ c;
+            b = block[i + 15] ^ a ;
             b = GF2(b);
-            *(state + 11) ^= b ^ c;
-
-            a   = *(state + 12);
-            c = *(state + 12) ^ *(state + 13) ^ *(state + 14) ^ *(state + 15);
-            b  = *(state + 12) ^ *(state + 13);
-            b = GF2(b);
-            *(state + 12) ^= b ^ c;
-            b  = *(state + 13) ^ *(state + 14);
-            b = GF2(b);
-            *(state + 13) ^= b ^ c;
-            b  = *(state + 14) ^ *(state + 15);
-            b = GF2(b);
-            *(state + 14) ^= b ^ c;
-            b  = *(state + 15) ^ a ;
-            b = GF2(b);
-            *(state + 15) ^= b ^ c;
+            block[i + 15] ^= b ^ c;
         }
         
         /**
