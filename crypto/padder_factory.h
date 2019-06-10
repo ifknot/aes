7f753742a499a9f0771dfe32ca26568f5a7e7b87
Jeremy Thornton <headwedge@hotmail.co.uk>
2019-06-10 23:06:14 +0100

    ANSI X9.23 and unit test

--- crypto/padder_factory.h
+++ crypto/padder_factory.h
@@ -19,7 +19,7 @@ namespace crypto {
      * @note PKCS#5 padding was only defined with (triple) DES operation in mind.
      */
     enum padder_mode_t {
-        PKCS7 //ToDo: ANSIX923, ONESZEROS, PKCS5
+        PKCS7, PKCS5, ANSIX923 //ToDo: ISO10126
     };
 
     /**
@@ -39,7 +39,7 @@ namespace crypto {
         using value_type  = T;
 
         /**
-         * @brief PKCS7 general purpose padding interface for writing pad values to an external container
+         * @brief PKCS7 padding scheme for writing pad values to an external container
          * If the block length is _B_then add _N_ padding bytes of value _N_ to make the input length up to the
          * next exact multiple of B.
          * @note If the input length is already an exact multiple of B then add B bytes of value B.
@@ -70,7 +70,7 @@ namespace crypto {
 
         /**
          * @brief After decrypting, check that the last N bytes of the decrypted data all have value N with 1 < N ≤ B.
-         * If so return nuber of bytes to strip, otherwise throw a decryption error.
+         * If so return number of bytes to strip, otherwise throw a decryption error.
          * @tparam Iterator
          * @param first
          * @param last
@@ -95,6 +95,76 @@ namespace crypto {
         }
 
     };
+
+    template<size_t BLOCK_SIZE,typename T>
+    struct padder<ANSIX923, BLOCK_SIZE, T> {
+
+        //ToDo: static_assert BLOCK_SIZE multiple of 8 and less than 2040
+
+        using value_type  = T;
+
+        /**
+         * @brief ANSO X 9.23 padding scheme for writing pad values to an external container
+         * ANSI X9.23, between 1 and 8 bytes are always added as padding.
+         * The block is padded with random bytes (although many implementations use 00) and the last byte of the block
+         * is set to the number of bytes added.
+         * @tparam Iterator
+         * @param first
+         * @param last
+         * @param out - iterator to a destination container with _at least_ block length space
+         * @return size_t the number of padding bytes written
+         */
+        template<typename ConstIterator, typename Iterator>
+        size_t pad(ConstIterator first, ConstIterator last, Iterator out) {
+            size_t tv= terminator(static_cast<size_t>(std::distance(first, last)));
+            for(auto i{tv - 1}; i--;) {
+                *out++ = 0x00;
+            }
+            *out = tv;
+            return tv;
+        }
+
+        /**
+         * @brief After decrypting, check that the last N -1  bytes of the decrypted data all have value 0x00 with 1 < N ≤ B.
+         * If so return number of bytes to strip, otherwise throw a decryption error.
+         * @tparam Iterator
+         * @param first
+         * @param last
+         * @return size_t the number of padding bytes to be stripped
+         */
+        template<typename ConstIterator>
+        size_t unpad(ConstIterator first, ConstIterator last) {
+            int i{*(last - 1)};
+            for(int i{*(last - 1)}; i > 1; i--) {
+                std::cout << "~~~~~~~~~~~  " << i << "\n";
+                if(*(last - i) != 0x00) {
+                    throw doh::cipher_exception(doh::UNPADDING);
+                }
+            }
+            return *(last - 1);
+        }
+
+        inline static padder_mode_t mode() {
+            return ANSIX923;
+        }
+
+        inline static size_t block_size() {
+            return BLOCK_SIZE;
+        }
+
+    private:
+
+        /**
+         * @brief terminator value
+         * @param data_size
+         * @return pad value - which can be used in a simple loop to pad the data container
+         */
+        inline size_t terminator(size_t data_size) {
+            return block_size() - data_size % block_size();
+        }
+
+    };
+
 
 }
 
--- tests/test_001_padders.cpp
+++ tests/test_001_padders.cpp
@@ -7,7 +7,7 @@
 
 TEST_CASE("Padder Factory", "[padder_factory]") {
 
-    SECTION("PKCS5") {
+    SECTION("PKCS7") {
         using padder_t = crypto::padder<>;
 
         std::vector<padder_t::value_type> plain =
@@ -17,7 +17,7 @@ TEST_CASE("Padder Factory", "[padder_factory]") {
                  0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a
                 };
 
-        padder_t pkcs5;
+        padder_t pkcs7;
 
         std::vector<padder_t::value_type> padding(padder_t::block_size());
 
@@ -25,22 +25,22 @@ TEST_CASE("Padder Factory", "[padder_factory]") {
             size_t temp;
             plain.push_back(plain[i]);
 
-                REQUIRE(pkcs5.mode() == crypto::PKCS5);
-                util::phex(plain);
-                temp = plain.size();
-                size_t n = pkcs5.pad(plain.begin(), plain.end(), padding.begin());
-                for(size_t j{0}; j < n; ++j) {
-                    plain.push_back(padding[j]);
-                }
-                util::phex(plain);
-                REQUIRE(plain.size() % pkcs5.block_size() == 0u);
+            REQUIRE(pkcs7.mode() == crypto::PKCS7);
+            util::phex(plain);
+            temp = plain.size();
+            size_t n = pkcs7.pad(plain.begin(), plain.end(), padding.begin());
+            for(size_t j{0}; j < n; ++j) {
+                plain.push_back(padding[j]);
+            }
+            util::phex(plain);
+            REQUIRE(plain.size() % pkcs7.block_size() == 0u);
 
 
-                REQUIRE(pkcs5.mode() == crypto::PKCS5);
-                util::phex(plain);
-                plain.resize(plain.size() - pkcs5.unpad(plain.begin(), plain.end()));
-                util::phex(plain);
-                REQUIRE(plain.size() == temp);
+            REQUIRE(pkcs7.mode() == crypto::PKCS7);
+            util::phex(plain);
+            plain.resize(plain.size() - pkcs7.unpad(plain.begin(), plain.end()));
+            util::phex(plain);
+            REQUIRE(plain.size() == temp);
 
         }
 
@@ -48,37 +48,88 @@ TEST_CASE("Padder Factory", "[padder_factory]") {
             size_t temp;
             plain.push_back(plain[i]);
 
-                REQUIRE(pkcs5.mode() == crypto::PKCS5);
-                util::phex(plain);
-                temp = plain.size();
-                size_t n = pkcs5.pad_value(plain.size());
-                for(size_t j{0}; j < n; ++j) {
-                    plain.push_back(static_cast<unsigned char &&>(n));
-                }
-                util::phex(plain);
-                REQUIRE(plain.size() % pkcs5.block_size() == 0u);
+            REQUIRE(pkcs7.mode() == crypto::PKCS7);
+            util::phex(plain);
+            temp = plain.size();
+            size_t n = pkcs7.pad_value(plain.size());
+            for(size_t j{0}; j < n; ++j) {
+                plain.push_back(static_cast<unsigned char &&>(n));
+            }
+            util::phex(plain);
+            REQUIRE(plain.size() % pkcs7.block_size() == 0u);
 
-                REQUIRE(pkcs5.mode() == crypto::PKCS5);
-                util::phex(plain);
-                plain.resize(plain.size() - pkcs5.unpad(plain.begin(), plain.end()));
-                util::phex(plain);
-                REQUIRE(plain.size() == temp);
+            REQUIRE(pkcs7.mode() == crypto::PKCS7);
+            util::phex(plain);
+            plain.resize(plain.size() - pkcs7.unpad(plain.begin(), plain.end()));
+            util::phex(plain);
+            REQUIRE(plain.size() == temp);
 
         }
 
         for(size_t j{1}; j <= padder_t::block_size(); ++j) {
 
-               REQUIRE(pkcs5.mode() == crypto::PKCS5);
-                size_t n = pkcs5.pad_value(plain.size());
-                for (size_t i{0}; i < n; ++i) {
-                    plain.push_back(static_cast<unsigned char &&>(n));
-                }
-                auto it = plain.end() - j;
-                (*it)--;
-                util::phex(plain);
-                CHECK_THROWS_AS(pkcs5.unpad(plain.begin(), plain.end()), doh::cipher_exception);
-                (*it)++;
-                plain.resize(plain.size() - pkcs5.unpad(plain.begin(), plain.end()));
+            REQUIRE(pkcs7.mode() == crypto::PKCS7);
+            size_t n = pkcs7.pad_value(plain.size());
+            for (size_t i{0}; i < n; ++i) {
+                plain.push_back(static_cast<unsigned char &&>(n));
+            }
+            auto it = plain.end() - j;
+            (*it)--;
+            util::phex(plain);
+            CHECK_THROWS_AS(pkcs7.unpad(plain.begin(), plain.end()), doh::cipher_exception);
+            (*it)++;
+            plain.resize(plain.size() - pkcs7.unpad(plain.begin(), plain.end()));
+        }
+
+    }
+
+    SECTION("ANSI X9.23") {
+        using padder_t = crypto::padder<crypto::ANSIX923>;
+
+        std::vector<padder_t::value_type> plain =
+                {0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a,
+                 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a,
+                 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a,
+                 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a
+                };
+
+        padder_t ansi;
+
+        std::vector<padder_t::value_type> padding(padder_t::block_size());
+
+        for(size_t i{0}; i < 16; ++i) {
+            size_t temp;
+            plain.push_back(plain[i]);
+
+            REQUIRE(ansi.mode() == crypto::ANSIX923);
+            util::phex(plain);
+            temp = plain.size();
+            size_t n = ansi.pad(plain.begin(), plain.end(), padding.begin());
+            for(size_t j{0}; j < n; ++j) {
+                plain.push_back(padding[j]);
+            }
+            util::phex(plain);
+            REQUIRE(plain.size() % ansi.block_size() == 0u);
+
+
+            REQUIRE(ansi.mode() == crypto::ANSIX923);
+            util::phex(plain);
+            plain.resize(plain.size() - ansi.unpad(plain.begin(), plain.end()));
+            util::phex(plain);
+            REQUIRE(plain.size() == temp);
+
+        }
+
+        size_t n = ansi.pad(plain.begin(), plain.end(), padding.begin());
+        for(size_t i{0}; i < n; ++i) {
+            plain.push_back(padding[i]);
+        }
+
+        for(size_t i{3}; i <= padder_t::block_size(); ++i) {
+            plain[plain.size() - i + 1] = 0x00;
+            plain[plain.size() - i] = 0xFF;
+            util::phex(plain);
+            CHECK_THROWS_AS(ansi.unpad(plain.begin(), plain.end()), doh::cipher_exception);
         }
 
     }
